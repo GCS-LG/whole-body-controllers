93e3293a3a45ef3fffb757f59776b30201002cf7
iCub <iCubGenova04@example.com>
2019-12-16 20:08:02 +0100

    Tune parameters on iCubGenova04

--- controllers/fixed-base-joints-control/app/robots/iCubGenova04/gainsAndReferences.m
+++ controllers/fixed-base-joints-control/app/robots/iCubGenova04/gainsAndReferences.m
@@ -8,17 +8,18 @@
 if Config.MOVE_JOINTS
     
     % Postural task gains
-    KP = 20*diag(ones(1,ROBOT_DOF));
-    KD = 2*sqrt(KP)*0.1;
-    KD(3,3) = 2*sqrt(KP(3,3))*0.2;
+    KP = 20*diag(ones(1,ROBOT_DOF)); 
+    KD = 2*sqrt(KP)*0.0;
+    KP(12:end,12:end) = 45*diag(ones(1,12));
+    KD(12:end,12:end) = 2*sqrt(KP(12:end,12:end))*0.25;
     
-%     Ref.Amplitude = 7.5*[0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 1 0 0, 0 0 0 0 0 0];
-%     Ref.Frequency = 0.5*[0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 1 0 0, 0 0 0 0 0 0];
+     Ref.Amplitude = 7.5*[0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0 1 1, 0 0 0 0 1 1];
+     Ref.Frequency = 0.5*[0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0 1 1, 0 0 0 0 1 1];
 
-     Ref.Amplitude = 3.5*ones(1,ROBOT_DOF);
-     Ref.Frequency = 0.5*ones(1,ROBOT_DOF);
-     Ref.Amplitude(1:11) = 0;
-     Ref.Frequency(1:11) = 0;
+%      Ref.Amplitude = 7.5*ones(1,ROBOT_DOF);
+%      Ref.Frequency = 0.5*ones(1,ROBOT_DOF);
+     Ref.Amplitude(1:3) = 0;
+     Ref.Frequency(1:3) = 0;
 
 else    
     % Postural task gains
